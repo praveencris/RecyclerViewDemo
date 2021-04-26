04859938e34a9bd08a98ba04fe8a34b5b8cb2a56
Praveen <pkchmits@gmail.com>
2021-04-26 16:14:25 +0530

    Changes w.r.t to click event on recyclerview done

--- app/src/main/java/com/example/recyclerviewdemoclick/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewdemoclick/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.recyclerviewdemoclick
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewdemoclick.adapter.NumberAdapter
+import com.example.recyclerviewdemoclick.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NumberAdapter.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var numberAdapter: NumberAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        numberAdapter = NumberAdapter(R.layout.list_item_alphabate, this)
+        val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter=numberAdapter
+        val list= (1..20).toMutableList()
+
+        numberAdapter.numbersList=list
+    }
+
+    override fun onAddClick(position: Int) {
+        val list = numberAdapter.numbersList;
+        list[position]=list[position] + 1
+        numberAdapter.numbersList = list
+        binding.recyclerView.scrollToPosition(position)
+    }
+
+    override fun onResetClick(position: Int) {
+        val list = numberAdapter.numbersList;
+        list[position]=0
+        numberAdapter.numbersList = list
+        binding.recyclerView.scrollToPosition(position)
+    }
+
+    override fun onTextClick(position: Int) {
+        val list = numberAdapter.numbersList;
+        Snackbar.make(binding.root, "Item Clicked ${list[position]}", Snackbar.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/recyclerviewdemoclick/adapter/NumberAdapter.kt
+++ app/src/main/java/com/example/recyclerviewdemoclick/adapter/NumberAdapter.kt
@@ -1,7 +1,66 @@
 package com.example.recyclerviewdemoclick.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewdemoclick.R
 
-class NumberAdapter():RecyclerView{
+class NumberAdapter(private val layoutId: Int, private val clickListener: OnClickListener) :
+    RecyclerView.Adapter<NumberAdapter.NumberViewHolder>() {
+    var numbersList: MutableList<Int> = mutableListOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder.create(parent, layoutId)
+    }
+
+    override fun getItemCount(): Int {
+        return numbersList.size
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        val item = numbersList[position]
+        holder.bind(item, clickListener)
+    }
+
+
+    class NumberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val itemText = itemView.findViewById<TextView>(R.id.numberText)
+        private val addImv = itemView.findViewById<ImageView>(R.id.addImv)
+        private val resetImv = itemView.findViewById<ImageView>(R.id.resetImv)
+
+
+        fun bind(item: Int, clickListener: OnClickListener) {
+            itemText.text = item.toString()
+            itemText.setOnClickListener {
+                clickListener.onTextClick(adapterPosition)
+            }
+            addImv.setOnClickListener {
+                clickListener.onAddClick(adapterPosition)
+            }
+            resetImv.setOnClickListener(View.OnClickListener {
+                clickListener.onResetClick(adapterPosition)
+            })
+        }
+
+        companion object {
+            fun create(parent: ViewGroup, layoutId: Int): NumberViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+                return NumberViewHolder(view)
+            }
+        }
+    }
+
+    interface OnClickListener {
+        fun onAddClick(position: Int)
+        fun onResetClick(position: Int)
+        fun onTextClick(position: Int)
+    }
 }
