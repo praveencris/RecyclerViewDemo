b36425f3d2a9479aa88845c3bd9db935f5e7dbe6
Praveen <pkchmits@gmail.com>
2021-04-26 21:35:47 +0530

    Changes done

--- app/src/main/java/com/example/recyclerviewdemoclick/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewdemoclick/MainActivity.kt
@@ -1,11 +1,96 @@
 package com.example.recyclerviewdemoclick
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.*
+import com.example.recyclerviewdemoclick.adapter.NumberAdapter
+import com.example.recyclerviewdemoclick.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,NumberAdapter.ClickListener{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var numberAdapter: NumberAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        numberAdapter = NumberAdapter(R.layout.list_item_alphabate,this)
+        binding.recyclerView.adapter = numberAdapter
+        binding.recyclerView.layoutManager = layoutManager
+
+        val numbersList = (1..20).toMutableList()
+        numberAdapter.numberList = numbersList
+
+
+        val itemTouchSimpleHelper= object : ItemTouchHelper.SimpleCallback(
+            0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                numberAdapter.numberList.removeAt(viewHolder.adapterPosition)
+                numberAdapter.notifyDataSetChanged()
+            }
+
+        }
+
+        val itemTouchHelper=ItemTouchHelper(itemTouchSimpleHelper)
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val list=numberAdapter.numberList
+        binding.recyclerView.layoutManager = when (item.itemId) {
+            R.id.linear_view -> {
+                numberAdapter=NumberAdapter(R.layout.list_item_alphabate,this)
+                LinearLayoutManager(this, RecyclerView.VERTICAL, false)}
+            R.id.grid_view -> {
+                numberAdapter=NumberAdapter(R.layout.list_item_number_grid,this)
+                GridLayoutManager(this, 2, RecyclerView.VERTICAL, false)
+            }
+            R.id.staggered_view -> {
+                numberAdapter=NumberAdapter(R.layout.list_item_number_grid,this)
+                StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+            }
+            else ->{
+                numberAdapter=NumberAdapter(R.layout.list_item_alphabate,this)
+                LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+            }
+        }
+
+        binding.recyclerView.adapter=numberAdapter
+        numberAdapter.numberList=list
+        return true
+    }
+
+    override fun addClicked(position: Int) {
+        numberAdapter.numberList[position]=(numberAdapter.numberList[position]+1)
+        numberAdapter.notifyDataSetChanged()
+    }
+
+    override fun resetClicked(position: Int) {
+        numberAdapter.numberList[position]=0
+        numberAdapter.notifyDataSetChanged()
+    }
+
+    override fun textClicked(position: Int) {
+        Snackbar.make(binding.root, "Number clicked: ${numberAdapter.numberList[position]}",Snackbar.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/recyclerviewdemoclick/adapter/NumberAdapter.kt
+++ app/src/main/java/com/example/recyclerviewdemoclick/adapter/NumberAdapter.kt
@@ -1,7 +1,63 @@
 package com.example.recyclerviewdemoclick.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewdemoclick.R
 
-class NumberAdapter():RecyclerView{
+class NumberAdapter(val layoutId: Int, val clickListener: ClickListener) :
+    RecyclerView.Adapter<NumberAdapter.ViewHolder>() {
+    var numberList = mutableListOf<Int>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemText = itemView.findViewById<TextView>(R.id.numberText)
+        val imvAdd = itemView.findViewById<ImageView>(R.id.addImv)
+        val imvReset = itemView.findViewById<ImageView>(R.id.resetImv)
+
+        fun bind(item: Int, clickListener: ClickListener) {
+            itemText.text = item.toString()
+            itemText.setOnClickListener {
+                clickListener.textClicked(adapterPosition)
+            }
+            imvAdd.setOnClickListener {
+                clickListener.addClicked(adapterPosition)
+            }
+            imvReset.setOnClickListener {
+                clickListener.resetClicked(adapterPosition)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return numberList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = numberList.get(position)
+        holder.bind(item, clickListener)
+    }
+
+
+    interface ClickListener {
+        fun addClicked(position: Int)
+        fun resetClicked(position: Int)
+        fun textClicked(position: Int)
+    }
 
 }
+
